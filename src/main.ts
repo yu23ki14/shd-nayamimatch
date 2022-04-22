4fbe2be3cc4863d986bc7745c7aaac4be4aa4aac
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-04-22 21:17:02 +0900

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(VueAxios, axios).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+
+		<p>
+			{{ testVariables }}
+		</p>
+		<button @click="changeTestVariables">テキストが変わる</button>
+
+		<ul>
+			<li v-for="pet in pets" :key="pet.id">
+				{{ pet.name }}
+			</li>
+		</ul>
+
+		<button @click="getValFromPetstore">ペットストアのテストAPI叩く</button>
+
+		<p v-for="ai in testArray" :key="ai">
+			{{ ai }}
+		</p>
+
+		<p>{{ testObject.key1 }}</p>
+		<p>{{ testObject.key2 }}</p>
+
+		<HelloWorld />
+	</div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+	import { Options, Vue } from 'vue-class-component'
+	import HelloWorld from '../components/HelloWorld.vue'
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+	@Options({
+		components: {
+			HelloWorld
+		}
+	})
+	export default class HomeView extends Vue {
+		testVariables = '適当な変数'
+		testArray = ['要素1', '要素2']
+		testObject = {
+			key1: 'val1',
+			key2: 'val2'
+		}
+		pets: { id: string; name: string }[] = []
+
+		changeTestVariables() {
+			this.testVariables = '変わったよ'
+		}
+
+		async getValFromPetstore() {
+			const { data: pets } = await this.axios.get(
+				'https://petstore.swagger.io/v2/pet/findByStatus?status=available'
+			)
+			this.pets = pets
+		}
+	}
 </script>
