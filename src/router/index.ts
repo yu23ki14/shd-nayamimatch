8a41e4d271e80c3e2f73f7793ba4b11a25156660
YuukiIshibashi <robopowerful.v@gmail.com>
2022-04-23 15:43:15 +0900

    vue-advanced-chat error

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import RoomView from "../views/RoomView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/room",
+    name: "room",
+    component: RoomView,
   },
   {
     path: "/about",
