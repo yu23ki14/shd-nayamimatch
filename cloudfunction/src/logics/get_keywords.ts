bc6ecebd88f7d90b980ce0a0a53d16e2260cffbd
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-05-11 11:00:06 +0900

    tmp

--- cloudfunction/src/logics/get_keywords.ts
+++ cloudfunction/src/logics/get_keywords.ts
@@ -1,6 +1,15 @@
 import {HttpFunction} from '@google-cloud/functions-framework';
+import language from '@google-cloud/language';
 
 export const getKeywords: HttpFunction = async (req, res) => {
+  const lc = new language.LanguageServiceClient();
+  const document: {content: string; type: 'PLAIN_TEXT'} = {
+    content: String(req.body.text),
+    type: 'PLAIN_TEXT',
+  };
+
+  const [result] = await lc.analyzeEntities({document: document});
+
   setTimeout(() => {
     res.set('Access-Control-Allow-Origin', '*');
     if (req.method === 'OPTIONS') {
